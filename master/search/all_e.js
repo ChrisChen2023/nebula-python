a2d6615357f6b34ea58373bb4f1e6b7d6909f327
ChrisChen2023 <chris.chen@vesoft.com>
2025-11-28 03:12:01 +0000

    Deploying to gh-pages from @ ChrisChen2023/nebula-python@4f060cf230ef87d71de4de3d8041678dd2ff6e86 🚀

--- master/search/all_e.js
+++ master/search/all_e.js
@@ -1,13 +1,16 @@
 var searchData=
 [
-  ['scan_5fedge_66',['scan_edge',['../d7/de1/classnebula3_1_1sclient_1_1GraphStorageClient_1_1GraphStorageClient.html#a58a9781ee7868da84312173edd82bbf6',1,'nebula3::sclient::GraphStorageClient::GraphStorageClient']]],
-  ['scan_5fedge_5fwith_5fpart_67',['scan_edge_with_part',['../d7/de1/classnebula3_1_1sclient_1_1GraphStorageClient_1_1GraphStorageClient.html#ab6d94d21a7ae92c22530432f4adba187',1,'nebula3::sclient::GraphStorageClient::GraphStorageClient']]],
-  ['scan_5fvertex_68',['scan_vertex',['../d7/de1/classnebula3_1_1sclient_1_1GraphStorageClient_1_1GraphStorageClient.html#a3cd2aa1c7870465c2607dbb1c0926669',1,'nebula3::sclient::GraphStorageClient::GraphStorageClient']]],
-  ['scan_5fvertex_5fwith_5fpart_69',['scan_vertex_with_part',['../d7/de1/classnebula3_1_1sclient_1_1GraphStorageClient_1_1GraphStorageClient.html#acf187f790ccfdb9bd65dd453c912af8d',1,'nebula3::sclient::GraphStorageClient::GraphStorageClient']]],
-  ['scanresult_70',['ScanResult',['../d0/ddf/classnebula3_1_1sclient_1_1ScanResult_1_1ScanResult.html',1,'nebula3::sclient::ScanResult']]],
-  ['session_71',['Session',['../d6/d27/classnebula3_1_1gclient_1_1net_1_1Session_1_1Session.html',1,'nebula3::gclient::net::Session']]],
-  ['session_5fcontext_72',['session_context',['../d8/d98/classnebula3_1_1gclient_1_1net_1_1ConnectionPool_1_1ConnectionPool.html#a9c153bf752f71d28dbd7e3acbae9a074',1,'nebula3::gclient::net::ConnectionPool::ConnectionPool']]],
-  ['sessionpool_73',['SessionPool',['../d7/d3a/classnebula3_1_1gclient_1_1net_1_1SessionPool_1_1SessionPool.html',1,'nebula3::gclient::net::SessionPool']]],
-  ['signout_74',['signout',['../df/d91/classnebula3_1_1gclient_1_1net_1_1Connection_1_1Connection.html#a1709bb5fe22faefaf911a7c29ae8915f',1,'nebula3::gclient::net::Connection::Connection']]],
-  ['spacecache_75',['SpaceCache',['../d2/d12/classnebula3_1_1mclient_1_1MetaCache_1_1SpaceCache.html',1,'nebula3::mclient::MetaCache']]]
+  ['scan_5fedge_0',['scan_edge',['../d7/de1/classnebula3_1_1sclient_1_1GraphStorageClient_1_1GraphStorageClient.html#abc1005de675b02ced7242ff3c79295de',1,'nebula3::sclient::GraphStorageClient::GraphStorageClient']]],
+  ['scan_5fedge_5fasync_1',['scan_edge_async',['../d7/de1/classnebula3_1_1sclient_1_1GraphStorageClient_1_1GraphStorageClient.html#a704eb8d697dacc311843e26db63b18b9',1,'nebula3::sclient::GraphStorageClient::GraphStorageClient']]],
+  ['scan_5fedge_5fwith_5fpart_2',['scan_edge_with_part',['../d7/de1/classnebula3_1_1sclient_1_1GraphStorageClient_1_1GraphStorageClient.html#a1685d4b4e480a59b979921337943fb9f',1,'nebula3::sclient::GraphStorageClient::GraphStorageClient']]],
+  ['scan_5fvertex_3',['scan_vertex',['../d7/de1/classnebula3_1_1sclient_1_1GraphStorageClient_1_1GraphStorageClient.html#afc1267a8c3a7884c871996b1ee55c73a',1,'nebula3::sclient::GraphStorageClient::GraphStorageClient']]],
+  ['scan_5fvertex_5fasync_4',['scan_vertex_async',['../d7/de1/classnebula3_1_1sclient_1_1GraphStorageClient_1_1GraphStorageClient.html#a57d0b72fd5fa59396c5d1a15b0f45862',1,'nebula3::sclient::GraphStorageClient::GraphStorageClient']]],
+  ['scan_5fvertex_5fwith_5fpart_5',['scan_vertex_with_part',['../d7/de1/classnebula3_1_1sclient_1_1GraphStorageClient_1_1GraphStorageClient.html#aec29f6111f8b27f5a6673eb9483a6685',1,'nebula3::sclient::GraphStorageClient::GraphStorageClient']]],
+  ['scanresult_6',['ScanResult',['../d0/ddf/classnebula3_1_1sclient_1_1ScanResult_1_1ScanResult.html',1,'nebula3::sclient::ScanResult']]],
+  ['session_7',['Session',['../d6/d27/classnebula3_1_1gclient_1_1net_1_1Session_1_1Session.html',1,'nebula3::gclient::net::Session']]],
+  ['session_5fcontext_8',['session_context',['../d8/d98/classnebula3_1_1gclient_1_1net_1_1ConnectionPool_1_1ConnectionPool.html#a185a207863b2ec38da3a336e5b570ba1',1,'nebula3::gclient::net::ConnectionPool::ConnectionPool']]],
+  ['sessionpool_9',['SessionPool',['../d7/d3a/classnebula3_1_1gclient_1_1net_1_1SessionPool_1_1SessionPool.html',1,'nebula3::gclient::net::SessionPool']]],
+  ['set_5fuser_5fpasswd_10',['set_user_passwd',['../d7/de1/classnebula3_1_1sclient_1_1GraphStorageClient_1_1GraphStorageClient.html#a25ad32d71597c3e07d85b9e4b6a49fc1',1,'nebula3::sclient::GraphStorageClient::GraphStorageClient']]],
+  ['signout_11',['signout',['../df/d91/classnebula3_1_1gclient_1_1net_1_1Connection_1_1Connection.html#a97aaa51639c0d494184fb4e4e46aa2af',1,'nebula3::gclient::net::Connection::Connection']]],
+  ['spacecache_12',['SpaceCache',['../d2/d12/classnebula3_1_1mclient_1_1MetaCache_1_1SpaceCache.html',1,'nebula3::mclient::MetaCache']]]
 ];
--- master/search/functions_9.js
+++ master/search/functions_9.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['open_146',['open',['../df/d91/classnebula3_1_1gclient_1_1net_1_1Connection_1_1Connection.html#a0e80bc37bfd49bf74aa3a6fd4f1760b7',1,'nebula3.gclient.net.Connection.Connection.open()'],['../d6/dcc/classnebula3_1_1mclient_1_1MetaClient.html#a6a7ca114eeea12e38e719a070358ae14',1,'nebula3.mclient.MetaClient.open()']]],
-  ['open_5fssl_147',['open_SSL',['../df/d91/classnebula3_1_1gclient_1_1net_1_1Connection_1_1Connection.html#a5731007ce223f2d0510e0a46022dacdc',1,'nebula3::gclient::net::Connection::Connection']]]
+  ['open_0',['open',['../df/d91/classnebula3_1_1gclient_1_1net_1_1Connection_1_1Connection.html#ac7c488e8f0f8fa8c9bf48bbd77b02f44',1,'nebula3.gclient.net.Connection.Connection.open()'],['../d6/dcc/classnebula3_1_1mclient_1_1MetaClient.html#a1b1cd51dff32421e012514f2b0123abd',1,'nebula3.mclient.MetaClient.open()']]],
+  ['open_5fssl_1',['open_SSL',['../df/d91/classnebula3_1_1gclient_1_1net_1_1Connection_1_1Connection.html#abe5dc7577bd7283273c2b22fb03909d1',1,'nebula3::gclient::net::Connection::Connection']]]
 ];
